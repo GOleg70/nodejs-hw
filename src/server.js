e7ba604c9b551c9ff25a3ae86f01ff2ac34551fc
Oleg Ganus <oleg110770@gmail.com>
2025-09-27 08:20:31 +0300

    end home work2

--- src/server.js
+++ src/server.js
@@ -34,8 +34,10 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
   res.status(200).json({
-    message: 'Retrieved note with ID: id_param',
+    message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
@@ -55,7 +57,6 @@ app.use((err, req, res, next) => {
   console.error('Error:', err.message);
   res.status(500).json({
     message: 'Internal Server Error',
-    error: err.message,
   });
 });
 
