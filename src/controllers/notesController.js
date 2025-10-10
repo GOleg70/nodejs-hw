89b81d40b204dd084e7c135002bdd321ed2bcf5c
Oleg Ganus <oleg110770@gmail.com>
2025-10-10 19:20:02 +0300

    Implement pagination, filtering, searching, and validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,55 +1,86 @@
+// src/controllers/notesController.js
 import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
-export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+export const getAllNotes = async (req, res, next) => {
+  const { page, perPage, tag, search } = req.query;
+
+  const skip = (page - 1) * perPage;
+
+  const filter = {};
+
+  if (tag) {
+    filter.tag = tag;
+  }
+
+  if (search) {
+    filter.$text = { $search: search };
+  }
+
+  const notesQuery = Note.find(filter);
+
+  try {
+    const [totalNotes, notes] = await Promise.all([
+      notesQuery.clone().countDocuments(),
+      notesQuery.skip(skip).limit(perPage).lean(),
+    ]);
+
+    const totalPages = Math.ceil(totalNotes / perPage);
+
+    res.status(200).json({
+      page: Number(page),
+      perPage: Number(perPage),
+      totalNotes,
+      totalPages,
+      notes,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
+
   const note = await Note.findById(noteId);
 
   if (!note) {
-    next(createHttpError(404, 'Note not found'));
-    return;
+    return next(createHttpError(404, 'Note not found'));
   }
 
   res.status(200).json(note);
 };
 
-// Новий контролер
-export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
-  res.status(201).json(note);
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json(note);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findOneAndDelete({
-    _id: noteId,
-  });
+  const note = await Note.findOneAndDelete({ _id: noteId });
 
   if (!note) {
-    next(createHttpError(404, 'Note not found'));
-    return;
+    return next(createHttpError(404, 'Note not found'));
   }
 
-  res.status(200).send(note);
+  res.status(200).json(note);
 };
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
 
-  const note = await Note.findOneAndUpdate(
-    { _id: noteId }, // Шукаємо по id
-    req.body,
-    { new: true }, // повертаємо оновлений документ
-  );
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   if (!note) {
-    next(createHttpError(404, 'Note not found'));
-    return;
+    return next(createHttpError(404, 'Note not found'));
   }
 
   res.status(200).json(note);
