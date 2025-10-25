71d9dc53d389fafef389a91d81b8a6b4c385c91e
Oleg Ganus <oleg110770@gmail.com>
2025-10-25 16:56:12 +0300

    уend home work 5

---
+++ src/controllers/userController.js
@@ -0,0 +1,24 @@
+// src/controllers/userController.js
+
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+
+export const updateUserAvatar = async (req, res, next) => {
+  if (!req.file) {
+    return next(createHttpError(400, 'No file'));
+  }
+
+  try {
+    const result = await saveFileToCloudinary(req.file.buffer);
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar: result.secure_url },
+      { new: true },
+    );
+
+    res.status(200).json({ url: user.avatar });
+  } catch {
+    return next(createHttpError(500, 'Failed to upload file to Cloudinary.'));
+  }
+};
